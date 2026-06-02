8c82b7e5199566903e802799d89458e3b6635257
Paul Watts <paul.watts@me.com>
2026-06-02 18:46:19 +1000

    Add WorkoutHandler for managing workout routes and functionality

--- api_project/internal/app/app.go
+++ api_project/internal/app/app.go
@@ -5,18 +5,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/PaulFWatts/complete_go/api_project/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 // NewApplication initializes and returns a new instance of the Application struct.
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// our stores will go here, but for now we can just initialize the handlers
+
+	// our handlers will go here
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 	return app, nil
 
